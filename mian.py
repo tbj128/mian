8d035780aed03b8eaf6b03149b73f91f91ccfbfa
Tom Jin <tbj128@gmail.com>
2016-05-31 20:26:08 -0700

    Config to run on externally accessible IP

--- mian.py
+++ mian.py
@@ -531,4 +531,5 @@ def getAllProjects(userID):
 if __name__ == '__main__':
 	app.secret_key = 'Twilight Sparkle'
 	app.config['SESSION_TYPE'] = 'filesystem'
-	app.run(debug=True, port=8080)
+	# app.run(debug=True, port=8080)
+	app.run(host='0.0.0.0', debug=True, port=8080)

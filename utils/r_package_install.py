5596cd3bd3c51e0412631367e60c1d98c871bc44
Tom Jin <tbj128@gmail.com>
2016-09-16 23:58:32 -0700

    Fixed improper indents

--- utils/r_package_install.py
+++ utils/r_package_install.py
@@ -4,10 +4,10 @@ from rpy2.robjects.packages import importr
 utils = importr('utils')
 
 def importr_custom(package_name):
-    try:
-        r_package = importr(package_name)
+	try:
+		r_package = importr(package_name)
 		print(package_name + ' is already installed')
-    except RRuntimeError:
+	except RRuntimeError:
 		utils.chooseCRANmirror(ind=1)
 		utils.install_packages(package_name)
 		print(package_name + ' has been installed')
